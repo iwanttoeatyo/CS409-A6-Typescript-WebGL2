b499944e9d7b697dae5aebfa2176b7f873da9917
Chris <boyd111c@gmail.com>
2018-03-13 23:15:24 -0600

    gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
 	entry: './src/main.ts',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'docs'),
 		filename: 'bundle.js',
 		publicPath: ''
 	},
